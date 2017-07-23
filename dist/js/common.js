397ecf14d21c7d7529223899117d8432f119513b
sshilenkov <sshilenkov10@gmail.com>
2017-07-23 19:14:38 +0300

    dist

--- dist/js/common.js
+++ dist/js/common.js
@@ -75,4 +75,101 @@ $(function() {
 		return false;
 	});
 
+	// Layers
+
+	$('.icon1').hover(
+		function() {
+			$('.description1').addClass('active');
+			$('.layer1').addClass('layer-active');
+			$('.layer2').addClass('layer-passive');
+			$('.layer3').addClass('layer-passive');
+			$('.layer4').addClass('layer-passive');
+		},
+		function() {
+			$('.layer1').removeClass('layer-active');
+			$('.layer2').removeClass('layer-passive');
+			$('.layer3').removeClass('layer-passive');
+			$('.layer4').removeClass('layer-passive');
+		});
+
+	$('.icon2').hover(
+		function() {
+			$('.icon1').removeClass('is-active');
+			$('.description1').removeClass('active');
+			$('.description2').addClass('active');
+			$('.layer1').addClass('layer-active');
+			$('.layer2').addClass('layer-passive');
+			$('.layer3').addClass('layer-passive');
+			$('.layer4').addClass('layer-passive');
+		},
+		function() {
+			$('.icon1').addClass('is-active');
+			$('.description1').addClass('active');
+			$('.description2').removeClass('active');
+			$('.layer1').removeClass('layer-active');
+			$('.layer2').removeClass('layer-passive');
+			$('.layer3').removeClass('layer-passive');
+			$('.layer4').removeClass('layer-passive');
+		});
+
+	$('.icon3').hover(
+		function() {
+			$('.icon1').removeClass('is-active');
+			$('.description1').removeClass('active');
+			$('.description3').addClass('active');
+			$('.layer2').addClass('layer-active');
+			$('.layer3').addClass('layer-passive');
+			$('.layer4').addClass('layer-passive');
+		},
+		function() {
+			$('.icon1').addClass('is-active');
+			$('.description1').addClass('active');
+			$('.description3').removeClass('active');
+			$('.layer2').removeClass('layer-active');
+			$('.layer3').removeClass('layer-passive');
+			$('.layer4').removeClass('layer-passive');
+		});
+
+	$('.icon4').hover(
+		function() {
+			$('.icon1').removeClass('is-active');
+			$('.description1').removeClass('active');
+			$('.description4').addClass('active');
+			$('.layer3').addClass('layer-active');
+			$('.layer4').addClass('layer-active');
+			$('.layer1').addClass('layer-passive');
+			$('.layer5').addClass('layer5-passive');
+		},
+		function() {
+			$('.icon1').addClass('is-active');
+			$('.description1').addClass('active');
+			$('.description4').removeClass('active');
+			$('.layer3').removeClass('layer-active');
+			$('.layer4').removeClass('layer-active');
+			$('.layer1').removeClass('layer-passive');
+			$('.layer5').removeClass('layer5-passive');
+		});
+
+	$('.icon5').hover(
+		function() {
+			$('.icon1').removeClass('is-active');
+			$('.description1').removeClass('active');
+			$('.description5').addClass('active');
+			$('.layer5').addClass('layer5-active');
+			$('.layer1').addClass('layer-passive');
+			$('.layer2').addClass('layer-passive');
+			$('.layer3').addClass('layer-passive');
+			$('.layer4').addClass('layer-passive');
+		},
+		function() {
+			$('.icon1').addClass('is-active');
+			$('.description1').addClass('active');
+			$('.description5').removeClass('active');
+			$('.layer5').removeClass('layer5-active');
+			$('.layer1').removeClass('layer-passive');
+			$('.layer2').removeClass('layer-passive');
+			$('.layer3').removeClass('layer-passive');
+			$('.layer4').removeClass('layer-passive');
+		});
+
 });
