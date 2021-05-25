39663fe7711a83595cb1304d59c62e0b97b3395f
sshilenkov <sshilenkov10@gmail.com>
2021-05-25 21:49:30 +0300

    New cabel site

---
+++ cabel/main.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+    var title = document.querySelector('.main__title');
+    var description = document.querySelector('.main__description');
+    var footer = document.querySelector('.footer');
+
+    title.classList.add('main__title--animate');
+    description.classList.add('main__description--animate');
+    footer.classList.add('footer--animate');
+}
